1fbf7afcd4f4ae8bc770653809ac459c7533a8c2
kup1dman <n.sinckevich1@gmail.com>
2024-05-03 11:51:04 +0300

    Remove explicit limit of commands because it is unnesecary

--- lib/folder_bot/parser.rb
+++ lib/folder_bot/parser.rb
@@ -1,7 +1,5 @@
 module FolderBot
   module Parser
-    AVAILABLE_TEXT_COMMANDS = %w[/start /done].freeze
-
     def command(data, type)
       Object.const_get parsed_message(data, type)
     rescue
@@ -12,7 +10,6 @@ module FolderBot
 
     def parsed_message(data, type)
       return nil unless data.start_with?('/')
-      return nil if type == :messages && !AVAILABLE_TEXT_COMMANDS.include?(data)
 
       data = '/pick_group' if data.match?(%r{^/pick_\w+$})
 
